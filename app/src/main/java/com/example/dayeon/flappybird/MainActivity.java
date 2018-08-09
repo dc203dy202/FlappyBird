db8233c4d3cc12bac6d2b42b24a2f7288caeed96
dc203dy202 <dc203dy202@gmail.com>
2018-08-09 09:41:40 +0900

    fly

--- app/src/main/java/com/example/dayeon/flappybird/MainActivity.java
+++ app/src/main/java/com/example/dayeon/flappybird/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.dayeon.flappybird;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    void startGame(View v){
+        Intent intent = new Intent(this, StartGame.class);
+        startActivity(intent);
+        finish();
     }
 }
